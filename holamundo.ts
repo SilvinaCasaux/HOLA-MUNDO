90af0d029035b7adeed774f7b82c5572206cc01b
silvinacasaux <silvina.casaux@gmail.com>
2023-03-19 20:07:12 -0300

    cree hola mundo

--- holamundo.ts
+++ holamundo.ts
@@ -1,1 +1,6 @@
 console.log("holamundo");
+console.log("hola");
+console.log("vamos a comer");
+console.log("un asado");
+console.log("en el parque");
+console.log("hola mundo;vamos a comer;un asado;en el parque");
